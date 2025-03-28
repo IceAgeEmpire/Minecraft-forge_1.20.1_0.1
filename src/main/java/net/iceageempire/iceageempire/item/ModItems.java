fddc3d91a034b6d1b5a9a9c8b7a66821da298f99
Harold II Jamiro <99066605+IceAgeEmpire@users.noreply.github.com>
2025-03-28 11:33:13 +0800

    Reimplemented Mod food items. Nerfed Diamond Potato

--- src/main/java/net/iceageempire/iceageempire/item/ModItems.java
+++ src/main/java/net/iceageempire/iceageempire/item/ModItems.java
@@ -1,15 +1,10 @@
 package net.iceageempire.iceageempire.item;
 
 import net.iceageempire.iceageempire.MyFirstMod;
-import net.iceageempire.iceageempire.item.custom.DiamondPotatoItem;
 import net.iceageempire.iceageempire.item.custom.MetalDetectorItem;
+import net.iceageempire.iceageempire.item.custom.ModFoods;
 import net.iceageempire.iceageempire.item.custom.RubySwordItem;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,12 +12,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final FoodProperties CUSTOM_FOOD_PROPERTIES = new FoodProperties.Builder()
-            .nutrition(6)         // Amount of hunger it restores (out of 20)
-            .saturationMod(0.8f)    // Saturation modifier
-            .alwaysEat()
-            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 1200, 100), 1.0f)
-            .build();
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MyFirstMod.MODID);
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties()));
@@ -32,7 +21,7 @@ public class ModItems {
     public static final RegistryObject<Item> RUBY_SWORD = ITEMS.register("ruby_sword",
             () -> new RubySwordItem(Tiers.DIAMOND, 1, -2.4f, new Item.Properties().durability(635)));
     public static final RegistryObject<Item> DIAMOND_POTATO = ITEMS.register("diamond_potato",
-            () -> new DiamondPotatoItem(new Item.Properties().food(CUSTOM_FOOD_PROPERTIES)));
+            () -> new Item(new Item.Properties().food(ModFoods.DIAMOND_POTATO)));
 
 
     public static void register (IEventBus eventbus){
---
+++ src/main/java/net/iceageempire/iceageempire/item/custom/ModFoods.java
@@ -0,0 +1,11 @@
+package net.iceageempire.iceageempire.item.custom;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties DIAMOND_POTATO = new FoodProperties.Builder().
+            nutrition(2).saturationMod(0.3f).alwaysEat()
+            .effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 18000, 1),1f).build();
+}
